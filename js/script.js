fed3b9ef3faf02d1e023aa189d67f03e0f8739e9
_smurf. <gabgingras@yahoo.com>
2024-05-16 14:48:55 -0400

    Adding Tab Icon + Tab Title

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// Author: dev-smurf
+'use strict';
+
+// Add event listener to the window
+window.addEventListener('DOMContentLoaded', Initialize);
+
+// Initialize the script
+function Initialize() {
+
+}
